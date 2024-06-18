10dac93b22ea257dc73f9f35b57722e66a2db2dc
mati1214 <28953166+mati1214@users.noreply.github.com>
2024-06-18 10:43:36 +0200

    Use AppCenter secret

--- app/src/main/java/ms/appcenter/sampleapp/android/MainActivity.java
+++ app/src/main/java/ms/appcenter/sampleapp/android/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
                     Analytics.class, Crashes.class, Distribute.class);
         } else {
             // Otherwise use the hardcoded string value here
-            AppCenter.start(getApplication(), "<APP SECRET HERE>",
+            AppCenter.start(getApplication(), "${{ secrets.APPCENTERSECRET }}",
                     Analytics.class, Crashes.class, Distribute.class);
         }
 
